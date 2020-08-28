02e9f645708e6e71555acc371efddc5b8bbf4fbd
Trivedhkumar <trivedhj@gmail.com>
2020-08-28 22:42:25 +0530

    updated with new files

--- app.js
+++ app.js
@@ -1,39 +1,31 @@
-let storeContent = document.querySelector('.text');
-let optionChoose = document.querySelector("select");
-updateDisplay();
+var express = require('express');
+var axios = require('axios');
+var fs = require("fs"); 
 
-optionChoose.onchange = () =>{
-    let optionContent = optionChoose.value || "all";
-    console.log(optionContent);
-    switch(optionContent){
-        case "all":
-            storeContent.textContent = "Fetching Data"
-            updateDisplay();
-            break;
+const port = 3000;
+app = express();
 
-        case "ex-duplicates":
-        case "se-duplicates":
-            storeContent.textContent = "No Duplicates found"
-            break;
+var storeContent = "";
 
-    }
-};
-function updateDisplay () {
-    var uniqueObjs = {};
+
+
+app.get("/",(req,res)=>{
+
     let  url = "https://o136z8hk40.execute-api.us-east-1.amazonaws.com/dev/get-list-of-conferences";
-    fetch(url).then(res=>{
-        res.json().then(text =>{
-             [...text.paid,...text.free].map(item => {
-                storeContent.textContent +=`\n\n\n`
-                for (let [key, value] of Object.entries(item)) {
-                storeContent.textContent += `${key}------------ ${value}\n`
-                }
-            });
-            // var data = [...text.paid,...text.free].filter((obj, pos, arr) => {
-            //     return arr.map(mapObj =>
-            //           mapObj.name).indexOf(obj.name) == pos;
-            //     });            
-        });
-    })
-    .catch(() => "error");
-}
+     axios.get(url).then(
+        data=> { 
+        jsondata =  data.data;
+
+                    [...jsondata.paid,...jsondata.free].map(item => {
+                       storeContent +=`\n\n\n`
+                       for (let [key, value] of Object.entries(item)) {
+                       storeContent += `[${key}: ${value}]\n`
+                       }
+                    })
+                    // console.log(storeContent);
+                    res.send(`${storeContent} \n`);
+
+}).catch(err => console.log("error"));
+
+});
+app.listen(port,() =>console.log("Server running"));
